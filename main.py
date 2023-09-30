b067927778ca5aab2d14b0e1c757ee872a6259d4
CCsatlos <c.csatlos@outlook.com>
2023-09-30 16:30:37 +0300

    add new function and while loop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list_last_todo = []
-choice = input('Enter your choice and press ENTER: ')
+list_todo = ['Homework', 'Shopping']
 
 
 def show_menu():
@@ -16,29 +15,50 @@ def last_todo():
         
     """Print the last 'ToDo' from the list."""
 
-    print(list_last_todo[-1])
+    print(list_todo[-1])
 
 
-def add_new_todo(new_add):
+def add_new_todo():
      
     """Add a new 'ToDo' to the list. 
             Parameters: 
                 new_add (str): """
 
     new_add = input('Enter your new ToDo: ')
-    return list_last_todo.append(new_add)
+    list_todo.append(new_add)
 
-# while choice != 3:
-#      if choice == 1:
-#           last_todo()
-#           show_menu()
-#      elif choice == 2: 
-#           add_new_todo(new_add)
-#           show_menu()
-#      else: 
-#           print('ERROR')
-#           show_menu()
 
+def user_choice():
+    """The function takes the input form keyboard.
+        Parameter:
+            choice (int): can be choose just 1, 2 or 3."""
+    choice = int(input('Enter your choice and press ENTER: '))
+    return choice
+
+
+
+show_menu()
+
+while True:
+    choice = user_choice()  # Get the user's choice once
+    if choice == 1:
+        last_todo()
+    elif choice == 2:
+        add_new_todo()
+    elif choice == 3:
+        break  
+    else:
+        print("Invalid choice. Please enter 1, 2, or 3.")
+
+ 
+    
+        
+
+
+
+
+        
+   
 
 
 
