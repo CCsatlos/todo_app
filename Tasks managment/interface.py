587d15f1b4ff320c239e43cfbf821206963ba2dd
CCsatlos <c.csatlos@outlook.com>
2023-11-19 20:57:36 +0200

    frames changed in labelframes

--- Tasks managment/interface.py
+++ Tasks managment/interface.py
@@ -1,45 +1,49 @@
 from tkinter import ttk
-from tkinter import Tk, Frame, Label, Button
+from tkinter import Tk, Frame, Label, Button, LabelFrame
 
 root = Tk()
-root.geometry("1100x800")
 root.title("Task Management")
-root.config(bg="white")
+root.config(bg = "white")
 root.resizable(1, 1)
 
+left_frame = Frame(root, name = "left frame")
+left_frame.grid(column = 0, row = 0)
+right_frame = Frame(root, name="right frame")
+right_frame.grid(column = 1, row = 0, sticky = "n")
 
-frame1 = Frame(root, background="#7FFFD4")
-frame1.grid(ipadx=100, ipady=390, padx=10, pady=10, sticky="nw")
-frame2 = Frame(root, background="#16E2F5")
-frame2.grid(ipadx=270, ipady=390,  padx=10, pady=10, row=0, column=1)
-frame2.pack_propagate(False)
+employees_label = LabelFrame(left_frame, text = "Employees")
+employees_label.grid(column = 0, row = 0, padx = 20, pady = 20)
+all_emp_button = Button(employees_label, text = "Select all employees").grid(column = 0, row = 0)
+select_button = Button(employees_label, text = "Select the employees").grid(column = 0, row = 1)
+delete_button = Button(employees_label, text = "Delete an employee").grid(column = 0, row = 2)
+
+for button in employees_label.winfo_children():
+    button.grid_configure(padx = 20, pady = 10, sticky = "ew")
+
+task_label = LabelFrame(left_frame, text = "Tasks")
+task_label.grid(column = 0, row = 1, padx = 20, pady = 20)
+add_task_button = Button(task_label, text = "Import the tasks").grid(column = 0, row = 0)
+man_task_button = Button(task_label, text = "Add a task manually").grid(column = 0, row = 1)
+del_task_button = Button(task_label, text = "Delete a task").grid(column = 0, row = 2)
+
+for button in task_label.winfo_children():
+    button.grid_configure(padx = 20, pady = 10, sticky = "ew")
+
+data_int_label = LabelFrame(left_frame, text = "Data logs")
+data_int_label.grid(column = 0, row = 2, padx = 20, pady = 20)
+logs_label = Label(data_int_label, text = "Should be display all logs").grid(column = 0, row = 0, padx=10, pady=10)
+
+planning = LabelFrame(right_frame, text="Planning")
+planning.grid(column = 0, row = 0, padx = 20, pady = 20)
+
+month_label = Label(planning, text = "Month/Day").grid(column = 0, row = 0)
+hour = Label(planning, text = "Hour").grid(column = 1, row = 0)
+task_name = Label(planning, text = "Task name").grid(column = 2, row = 0)
+name = Label(planning, text = "Employee").grid(column = 3, row = 0)
+target_date = Label(planning, text = "Target date").grid(column = 4, row = 0)
 
 
-
-the_button = Button(frame1, text="Button", background = "#16E2F5")
-the_button.grid(ipadx=50, ipady=5, padx=10, pady=10, sticky="n")
-
-row_date = 0
-row_hour = 0
-row_name = 0
-
-
-
-for date_frame in range(0, 10):
-    date_frame = Frame(frame2, background="white")
-    date_frame.grid(ipadx=70, ipady=10, padx=5, pady=5, row=row_date, column=0)
-    row_date += 1
-
-for name_frame in range(0, 10):
-    date_frame = Frame(frame2, background="white")
-    date_frame.grid(ipadx=40, ipady=10, padx=5, pady=5, row=row_hour, column=1)
-    row_hour += 1
-
-for name_frame in range(0, 10):
-    date_frame = Frame(frame2, background="white")
-    date_frame.grid(ipadx=100, ipady=10, padx=5, pady=5, row=row_name, column=2)
-    row_name += 1
+for widget in planning.winfo_children():
+    widget.grid(padx=50)
     
-
-
 root.mainloop()
