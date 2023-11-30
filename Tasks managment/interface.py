3b016cb147131e4d2ed4654f1c2c7e6734d9edfc
CCsatlos <c.csatlos@outlook.com>
2023-11-30 10:30:54 +0200

    using classes instead variables

--- Tasks managment/interface.py
+++ Tasks managment/interface.py
@@ -1,78 +1,94 @@
 from tkinter import ttk
 from tkinter import Tk, Frame, Label, Button, LabelFrame, Toplevel, Entry
 
-#creating the root
 root = Tk()
-root.title("Task Management")
+root.title("Task Manager")
 root.config(bg = "white")
-root.resizable(0, 0)
+# root.resizable(0, 0)
 
-#establishing function for employees list
-def employee_window():
-    new_window = Toplevel(add_del_button)
-    new_window.title("Employee window")
-    new_window.geometry("450x200+160+120")
+class MainFrames:
 
-    name = Label(new_window, text = "First and last name").grid(column = 0, row = 0, padx = 10, pady = 10)
-    hour = Label(new_window, text = "Working hours per day").grid(column = 0, row = 1, padx = 10, pady = 10)
-    first_name_entry = Entry(new_window).grid(column = 1, row = 0, padx = 10, pady = 10)
-    second_name_entry = Entry(new_window).grid(column = 2, row = 0, padx = 10, pady = 10)
-    hours_entry = Entry(new_window).grid(column = 1, row = 1, padx = 10, pady = 10)
-    save_button = Button(new_window, text = "Save").grid(column = 1, row = 2, ipadx = 10, pady=10)
+    def __init__(self, master):
+        self.left_frame = Frame(master, name = "left frame")
+        self.left_frame.grid(column = 0, row = 0)
+        self.right_frame = Frame(master, name = "right frame")
+        self.right_frame.grid(column = 1, row = 0, sticky = "n")
+        self.labels()
 
-#creating the two main frames for interface
-left_frame = Frame(root, name = "left frame")
-left_frame.grid(column = 0, row = 0)
-right_frame = Frame(root, name="right frame")
-right_frame.grid(column = 1, row = 0, sticky = "n")
+    def labels(self):
+        label_1 = EmployeesLabel(self.left_frame)
+        label_2 = TaskLabel(self.left_frame)
+        label_3 = LogsFrame(self.left_frame)
+        label_4 = PlaningLabel(self.right_frame)
 
-#setting the left frame
-employees_label = LabelFrame(left_frame, text = "Employees")
-employees_label.grid(column = 0, row = 0, padx = 20, pady = 20)
-all_emp_button = Button(employees_label, text = "Select all employees").grid(column = 0, row = 0)
-select_button = Button(employees_label, text = "Select the employees").grid(column = 0, row = 1)
-add_del_button = Button(employees_label, text = "Delete/delete an employee", command=employee_window).grid(column = 0, row = 2)
+class EmployeesLabel:
 
-#setting the position of all available widgets into the left frame
-for button in employees_label.winfo_children():
-    button.grid_configure(padx = 20, pady = 10, sticky = "ew")
-
-#setting the second/right frame
-task_label = LabelFrame(left_frame, text = "Tasks")
-task_label.grid(column = 0, row = 1, padx = 20, pady = 20)
-add_task_button = Button(task_label, text = "Import the tasks").grid(column = 0, row = 0)
-man_task_button = Button(task_label, text = "Add a task manually").grid(column = 0, row = 1)
-del_task_button = Button(task_label, text = "Delete a task").grid(column = 0, row = 2)
-
-#setting the position of all available widgets into the left frame
-for button in task_label.winfo_children():
-    button.grid_configure(padx = 20, pady = 10, sticky = "ew")
-
-#continuing with labels frame
-data_int_label = LabelFrame(left_frame, text = "Data logs")
-data_int_label.grid(column = 0, row = 2, padx = 20, pady = 20)
-logs_label = Label(data_int_label, text = "Should be display the last few logs").grid(column = 0, row = 0, padx=10, pady=10)
-
-#setting the right frame
-planning = LabelFrame(right_frame, text="Planning")
-planning.grid(column = 0, row = 0, padx = 20, pady = 20)
-
-#creating the labels for the right frame
-month_label = Label(planning, text = "Month/Day")
-hour = Label(planning, text = "Hour")
-task_name = Label(planning, text = "Task name")
-name = Label(planning, text = "Employee")
-target_date = Label(planning, text = "Target date")
-
-#setting the geometry of all available widgets into the right children frame
-w_colum = 0
-
-for widget in planning.winfo_children():
-    widget.grid(column = w_colum, row = 0)
-    w_colum += w_colum + 1
-
-#positioning the labels into the right right frame
-for widget in planning.winfo_children():
-    widget.grid(padx=50)
+    def __init__(self, master):
+        self.emp_label = LabelFrame(master, text = "Employees")
+        self.emp_label.grid(column = 0, row = 0, padx = 20, pady = 20)
+        self.all_emp_button = Button(self.emp_label, text = "Select all employees")
+        self.select_button = Button(self.emp_label, text = "Select the employees")
+        self.add_del_button = Button(self.emp_label, text = "Add/delete an employee", command = self.open_window)
+        self.placing_buttons()
     
+    def placing_buttons(self):
+        place = 0
+        for button in self.emp_label.winfo_children():
+            button.grid(column = 0, row = place, padx = 20, pady = 10, sticky = "ew")
+            place += place + 1
+
+    def open_window(self):
+        new_window = Add_Del_Button(self.add_del_button)
+
+class Add_Del_Button:
+
+    def __init__(self, master):
+        self.top_lvl = Toplevel(master)
+        self.top_lvl.geometry("450x200+160+120")
+
+class TaskLabel:
+
+    def __init__(self, master):
+        self.task_label = LabelFrame(master, name = "task label")
+        self.task_label.grid(column = 0, row = 1, padx = 20, pady = 20)
+        self.import_task = Button(self.task_label, text = "Import the tasks")
+        self.add_task = Button(self.task_label, text = "add a tasks")
+        self.del_task = Button(self.task_label, text = "Delete a tasks")
+
+        self.placing_buttons()
+    
+    def placing_buttons(self):
+        buttons = [self.import_task, self.add_task, self.del_task]
+        for place, button in enumerate(buttons):
+            button.grid(column = 0, row = place, padx = 20, pady = 10, sticky = "ew")
+
+class LogsFrame:
+
+    def __init__(self, master):
+        self.logs_frame = LabelFrame(master, text = "Data logs")
+        self.logs_frame.grid(column = 0, row = 2, padx = 20, pady = 20)
+        self.logs_label = Label(self.logs_frame, text = "Should be display the last few logs")
+        self.logs_label.grid(column = 0, row = 0, padx=10, pady=10)
+
+class PlaningLabel:
+    def __init__(self, master):
+        self.planing = LabelFrame(master, text = "Planing")
+        self.planing.grid(column = 1, row = 0, padx=20, pady=20)
+        self.month_label = Label(self.planing, text = "Month/Day")
+        self.hour = Label(self.planing, text = "Hour")
+        self.task_name = Label(self.planing, text = "Task name")
+        self.name = Label(self.planing, text = "Employee")
+        self.target_date = Label(self.planing, text = "Target date")
+        self.placing_labels()
+    
+    def placing_labels(self):
+        labels = [self.month_label, self.hour, self.task_name, self.name, self.target_date]
+        for place, label in enumerate(labels):
+            label.grid(column=place, row=0, padx=50, pady=20)
+
+frame1 = MainFrames(root)
 root.mainloop()
+
+
+
+        
